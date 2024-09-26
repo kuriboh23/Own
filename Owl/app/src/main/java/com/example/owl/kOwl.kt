c392c7fc461144282ee3b44dd972d5adbf866ca7
ayanokouji <osathenas23@gmail.com>
2024-09-26 15:32:52 +0100

    Class File

--- Owl/app/src/main/java/com/example/owl/kOwl.kt
+++ Owl/app/src/main/java/com/example/owl/kOwl.kt
@@ -1,5 +1,16 @@
 package com.example.owl
 
+class Animal(var nom: String,var age:Int){
+    fun afficherDetails(){
+        println("$nom have $age years old!")
+    }
+}
+
 fun main(){
-    println(95)
+    val cat = Animal("Kitty",8)
+    val dog = Animal("Toby",6)
+
+    cat.afficherDetails()
+    dog.afficherDetails()
+
 }
