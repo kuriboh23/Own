f383371afefc7891a985e51d85665f171c349c66
ayanokouji <osathenas23@gmail.com>
2024-09-26 16:05:23 +0100

    EX3 File

---
+++ Owl/app/src/main/java/TD_K/Ex3.kt
@@ -0,0 +1,35 @@
+package TD_K
+
+class CompteBancaire(var titulaire:String, var solde:Double){
+
+    fun depot(s:Double){
+        afficherSolde()
+        solde += s
+        println("You deposit $s$. \n|-- Your balance: $solde\$ --|")
+    }
+    fun retrait(r:Double){
+        if (solde > 0){
+            if(solde>=r){
+                afficherSolde()
+                solde -= r
+                println("You withdraw $r$. \n|-- Your balance: $solde\$ --|")
+            }else println("Your balance $solde is less than your withdraw $r")
+        }else println("Sorry, you don't have enough balance --> $solde$")
+    }
+
+    private fun afficherSolde(){
+        println("|--- Your balance: $solde\$ ---|")
+    }
+
+
+}
+
+fun main(){
+    val cmpt = CompteBancaire("CIH",10.0)
+
+    cmpt.retrait(10.0)
+    cmpt.retrait(10.0)
+    cmpt.depot(5.0)
+    cmpt.retrait(10.0)
+    cmpt.retrait(05.0)
+}
