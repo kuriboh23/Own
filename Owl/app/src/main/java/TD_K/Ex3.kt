7bf4e39b55cdb1a9969d68b9bf83f45be15ae92b
ayanokouji <osathenas23@gmail.com>
2024-09-29 14:12:35 +0100

    Class File

--- Owl/app/src/main/java/TD_K/Ex3.kt
+++ Owl/app/src/main/java/TD_K/Ex3.kt
@@ -1,18 +1,33 @@
 package TD_K
 
 class CompteBancaire(var titulaire:String, var solde:Double){
+    companion object {
+        var globalId = 0
+    }
+    var id = 0
+
+    constructor(titulaire: String,solde: Int):this(titulaire,solde.toDouble())
+
+    init {
+        globalId++
+        id = globalId
+    }
 
     fun depot(s:Double){
         afficherSolde()
         solde += s
+        println("Your ID is: $id")
         println("You deposit $s$. \n|-- Your balance: $solde\$ --|")
     }
     fun retrait(r:Double){
         if (solde > 0){
             if(solde>=r){
                 afficherSolde()
+                println("Your ID is: $id")
                 solde -= r
-                println("You withdraw $r$. \n|-- Your balance: $solde\$ --|")
+                println("You withdraw $r$.")
+                afficherSolde()
+                println()
             }else println("Your balance $solde$ is less than your withdraw $r")
         }else println("Sorry, you don't have enough balance --> $solde$")
     }
@@ -25,10 +40,9 @@ class CompteBancaire(var titulaire:String, var solde:Double){
 
 fun main(){
     val cmpt = CompteBancaire("CIH",10.0)
+    val cmpt2 =CompteBancaire("BMCE",20)
 
     cmpt.retrait(10.0)
-//    cmpt.retrait(10.0)
-//    cmpt.depot(5.0)
-//    cmpt.retrait(10.0)
-//    cmpt.retrait(05.0)
+    cmpt2.retrait(10.0)
+    cmpt.depot(10.0)
 }
